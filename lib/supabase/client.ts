72a79476c222e8845c3eea85c5100969baa783da
Addyyy008 <addyjaiswal007@gmail.com>
2026-02-17 23:20:05 +0530

    fix

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,6 +4,12 @@ import { Database } from "../database.types";
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      realtime: {
+        params: {
+          eventsPerSecond: 10,
+        },
+      },
+    }
   );
-}
